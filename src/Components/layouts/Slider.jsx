fbb501e92fc0f461c940f64a842e972472a1d9c0
Bugra <seyitbugraerden@gmail.com>
2024-02-19 12:57:05 +0300

    Components

--- src/Components/layouts/Slider.jsx
+++ src/Components/layouts/Slider.jsx
@@ -2,75 +2,86 @@ import React from "react";
 
 function Slider() {
   return (
-    <div class="section cc-store-home-wrap">
-      <div class="intro-header">
-        <div class="intro-content cc-homepage">
-          <div class="intro-text">
-            <div class="heading-jumbo">Grow your business.</div>
-            <div class="paragraph-bigger cc-bigger-white-light">
-              Give your business a boost with a beautifully crafted homepage.
+    <div className="section cc-store-home-wrap">
+      <div
+        className="intro-header"
+        style={{
+          backgroundImage:
+            "linear-gradient(transparent, rgb(0,0,0,.5)), url(https://img3.aksam.com.tr/imgsdisk/2021/04/26/t25_formula-1de-yeni-donem-sp-955.jpg)",
+          backgroundAttachment: "fixed",
+          backgroundPositionY: "-150px",
+        }}
+      >
+        <div className="intro-content cc-homepage">
+          <div className="intro-text">
+            <div className="heading-jumbo">Race Day Insights</div>
+            <div className="paragraph-bigger cc-bigger-white-light">
+              Get behind-the-scenes analysis and expert commentary on the latest
+              F1 races.
               <br />
             </div>
           </div>
           <a
             href="/about"
-            class="button cc-jumbo-button cc-jumbo-white w-inline-block"
+            className="button cc-jumbo-button cc-jumbo-white w-inline-block"
           >
             <div>Learn More</div>
           </a>
         </div>
       </div>
-      <div class="container">
-        <div class="motto-wrap">
-          <div class="label cc-light">What we believe in</div>
-          <div class="heading-jumbo-small">
-            Grow your business, establish your brand, and put your customers
-            first.
+      <div className="container">
+        <div className="motto-wrap">
+          <div className="label cc-light">Who is Here</div>
+          <div className="heading-jumbo-small">
+            Brand Spotlight: Powerhouses of the Circuit
             <br />
           </div>
         </div>
-        <div class="divider"></div>
-        <div class="home-content-wrap">
-          <div class="w-layout-grid about-grid">
+        <div className="divider"></div>
+        <div className="home-content-wrap">
+          <div className="w-layout-grid about-grid">
             <div id="w-node-_86e64837-0616-515b-4568-76c147234d34-530bfaf3">
-              <div class="home-section-wrap">
-                <div class="label cc-light">About</div>
-                <h2 class="section-heading">Who we are</h2>
-                <p class="paragraph-light">
-                  Nulla vel sodales tellus, quis condimentum enim. Nunc
-                  porttitor venenatis feugiat. Etiam quis faucibus erat, non
-                  accumsan leo. Aliquam erat volutpat. Vestibulum ac faucibus
-                  eros. Cras ullamcorper gravida tellus ut consequat.
+              <div className="home-section-wrap">
+                <div className="label cc-light">Most Winner</div>
+                <h2 className="section-heading">Mercedes-AMG</h2>
+                <p className="paragraph-light">
+                  Mercedes-AMG Petronas Formula One Team is a powerhouse in
+                  Formula 1, renowned for its dominance in the sport's modern
+                  era. With cutting-edge technology, top-tier engineering, and a
+                  roster of world-class drivers, Mercedes-AMG consistently
+                  pushes the boundaries of speed and innovation on the race
+                  track.
                 </p>
               </div>
-              <a href="/about" class="button w-inline-block">
+              <a href="/about" className="button w-inline-block">
                 <div>Learn More</div>
               </a>
             </div>
             <img
-              src="https://assets-global.website-files.com/65d25ec9405b7cdf530bfabd/65d25ec9405b7cdf530bfb1d_placeholder%203.svg"
+              src="https://i.pinimg.com/originals/e1/1c/c8/e11cc8ad8bff2641e0076ad00e4beb98.jpg"
               id="w-node-_86e64837-0616-515b-4568-76c147234d3f-530bfaf3"
               alt=""
             />
           </div>
-          <div class="w-layout-grid about-grid cc-about-2">
+          <div className="w-layout-grid about-grid cc-about-2">
             <div id="w-node-_86e64837-0616-515b-4568-76c147234d41-530bfaf3">
-              <div class="home-section-wrap">
-                <div class="label cc-light">Team</div>
-                <h2 class="section-heading">What we do</h2>
-                <p class="paragraph-light">
-                  Nulla vel sodales tellus, quis condimentum enim. Nunc
-                  porttitor venenatis feugiat. Etiam quis faucibus erat, non
-                  accumsan leo. Aliquam erat volutpat. Vestibulum ac faucibus
-                  eros. Cras ullamcorper gravida tellus ut consequat.
+              <div className="home-section-wrap">
+                <div className="label cc-light">Most Popular</div>
+                <h2 className="section-heading">Scuderia Ferrari</h2>
+                <p className="paragraph-light">
+                  Ferrari is an iconic name in Formula 1, synonymous with
+                  passion, tradition, and success. With a storied history
+                  spanning decades, the Scuderia Ferrari team continues to
+                  captivate fans worldwide with its distinctive red cars and
+                  unwavering pursuit of victory on the race track.
                 </p>
               </div>
-              <a href="/team" class="button w-inline-block">
+              <a href="/team" className="button w-inline-block">
                 <div>Learn More</div>
               </a>
             </div>
             <img
-              src="https://assets-global.website-files.com/65d25ec9405b7cdf530bfabd/65d25ec9405b7cdf530bfb1f_placeholder%201.svg"
+              src="https://www.mostlyf1.com/wp-content/uploads/2019/09/Charles-Leclerc-Italy-2019-Podium.jpg"
               id="w-node-_86e64837-0616-515b-4568-76c147234d4c-530bfaf3"
               alt=""
             />
