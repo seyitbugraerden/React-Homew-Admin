21dcb1ab36a82888b29f05b8a240aed90a5fb12f
Bugra <seyitbugraerden@gmail.com>
2024-02-19 18:23:39 +0300

    Dynamic Render Side

--- src/Components/layouts/Header.jsx
+++ src/Components/layouts/Header.jsx
@@ -5,6 +5,8 @@ import { useSelector } from "react-redux";
 
 function Header() {
   const navItem = useSelector((state) => state.admin.navbarItems);
+  const logo = useSelector((state) => state.admin.logo);
+  const headerButton = useSelector((state) => state.admin.headerButton);
 
   return (
     <div
@@ -42,19 +44,14 @@ function Header() {
             ))}
           </nav>
           <div className="menu-button w-nav-button">
-            <img
-              src="https://assets-global.website-files.com/65d25ec9405b7cdf530bfabd/65d25ec9405b7cdf530bfb1a_menu-icon.png"
-              width="22"
-              alt=""
-              className="menu-icon"
-            />
+            <img src={logo} width="22" alt="" className="menu-icon" />
           </div>
         </div>
         <a
           href="mailto:mail@business.com?subject=You&#x27;ve%20got%20mail!"
           className="button cc-contact-us w-inline-block"
         >
-          <div>Contact us</div>
+          <div>{headerButton}</div>
         </a>
       </div>
     </div>
