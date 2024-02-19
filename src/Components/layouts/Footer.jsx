38b7f7e72dfd98921be3831f0403a3270829a27c
Bugra <seyitbugraerden@gmail.com>
2024-02-19 13:36:15 +0300

    Classes Fixed

--- src/Components/layouts/Footer.jsx
+++ src/Components/layouts/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <>
       <div
-        class="section cc-cta"
+        className="section cc-cta"
         style={{
           backgroundImage:
             "linear-gradient(rgb(0,0,0,.5), rgb(0,0,0,.5)),url(https://wallpapercave.com/wp/wp9200426.jpg)",
@@ -13,43 +13,46 @@ function Footer() {
           color: "white",
         }}
       >
-        <div class="container">
-          <div class="cta-wrap">
+        <div className="container">
+          <div className="cta-wrap">
             <div>
-              <div class="cta-text">
-                <div class="heading-jumbo-small">
+              <div className="cta-text">
+                <div className="heading-jumbo-small">
                   F1 Insider Access: Your Ticket to Exclusive Race Updates
                   <br />
                 </div>
-                <div class="paragraph-bigger cc-bigger-light">
+                <div className="paragraph-bigger cc-bigger-light">
                   Subscribe now to unlock premium notifications and in-depth
                   race insights. Get ahead of the curve with insider access to
                   all the action on and off the track!
                   <br />
                 </div>
               </div>
-              <a href="/contact" class="button cc-jumbo-button w-inline-block">
+              <a
+                href="/contact"
+                className="button cc-jumbo-button w-inline-block"
+              >
                 <div>Subscribe Now</div>
               </a>
             </div>
           </div>
         </div>
       </div>
-      <div class="section">
-        <div class="container">
-          <div class="footer-wrap">
+      <div className="section">
+        <div className="container">
+          <div className="footer-wrap">
             <a
               href="https://github.com/seyitbugraerden"
               target="_blank"
-              class="webflow-link w-inline-block"
+              className="webflow-link w-inline-block"
             >
               <img
                 src="https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png"
                 width="15"
                 alt=""
-                class="webflow-logo-tiny"
+                className="webflow-logo-tiny"
               />
-              <div class="paragraph-tiny">github.com/seyitbugraerden</div>
+              <div className="paragraph-tiny">github.com/seyitbugraerden</div>
             </a>
           </div>
         </div>
--- src/Components/layouts/News.jsx
+++ src/Components/layouts/News.jsx
@@ -2,38 +2,38 @@ import React from "react";
 
 function News() {
   return (
-    <div class="section">
-      <div class="container">
-        <div class="blog-heading">
-          <div class="label cc-light">News</div>
-          <h2 class="work-heading">Latest News</h2>
+    <div className="section">
+      <div className="container">
+        <div className="blog-heading">
+          <div className="label cc-light">News</div>
+          <h2 className="work-heading">Latest News</h2>
         </div>
-        <div class="collection-list-wrapper w-dyn-list">
-          <div role="list" class="collection-wrap w-dyn-items">
-            <div role="listitem" class="blog-preview-wrap w-dyn-item">
+        <div className="collection-list-wrapper w-dyn-list">
+          <div role="list" className="collection-wrap w-dyn-items">
+            <div role="listitem" className="blog-preview-wrap w-dyn-item">
               <a
                 href="/post/10-quick-tips-about-blogging"
-                class="business-article-heading"
+                className="business-article-heading"
               >
                 New Regulations Unveiled for 2024 Season
               </a>
-              <div class="label cc-blog-date">Jan 24, 2024</div>
-              <p class="paragraph-light">
+              <div className="label cc-blog-date">Jan 24, 2024</div>
+              <p className="paragraph-light">
                 F1 governing body, the FIA, has revealed significant changes to
                 the technical regulations for the 2024 season, aiming to improve
                 racing and reduce costs. These alterations include tweaks to
                 aerodynamics, standardized parts, and sustainability measures.
               </p>
             </div>
-            <div role="listitem" class="blog-preview-wrap w-dyn-item">
+            <div role="listitem" className="blog-preview-wrap w-dyn-item">
               <a
                 href="/post/15-best-blogs-to-follow-about-web-design"
-                class="business-article-heading"
+                className="business-article-heading"
               >
                 Driver Transfers Shake Up Grid
               </a>
-              <div class="label cc-blog-date">Jan 21, 2024</div>
-              <p class="paragraph-light">
+              <div className="label cc-blog-date">Jan 21, 2024</div>
+              <p className="paragraph-light">
                 Several high-profile driver transfers have occurred ahead of the
                 2024 season, with notable moves including Lewis Hamilton's
                 switch to Aston Martin and Max Verstappen joining Ferrari. These
@@ -41,15 +41,15 @@ function News() {
                 upcoming championship.
               </p>
             </div>
-            <div role="listitem" class="blog-preview-wrap w-dyn-item">
+            <div role="listitem" className="blog-preview-wrap w-dyn-item">
               <a
                 href="/post/7-of-the-best-examples-of-beautiful-blog-design"
-                class="business-article-heading"
+                className="business-article-heading"
               >
                 Expansion Plans for F1 Calendar
               </a>
-              <div class="label cc-blog-date">Jan 21, 2024</div>
-              <p class="paragraph-light">
+              <div className="label cc-blog-date">Jan 21, 2024</div>
+              <p className="paragraph-light">
                 Formula 1 is exploring new venues and markets for its race
                 calendar, with discussions underway to potentially add races in
                 countries like Indonesia and Qatar. These expansion plans aim to
