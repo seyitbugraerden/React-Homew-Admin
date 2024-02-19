ce41a7f40e3ed553d972245f3523d355b7734c9a
Bugra <seyitbugraerden@gmail.com>
2024-02-20 01:50:57 +0300

    admin update

--- src/Components/pages/Admin.jsx
+++ src/Components/pages/Admin.jsx
@@ -1,17 +1,61 @@
 import React, { useState } from "react";
-
+import "../../Admin.css";
+import { MenuFoldOutlined } from "@ant-design/icons";
+import { UserOutlined } from "@ant-design/icons";
+import { Avatar } from "antd";
 function Admin() {
-  const [value, setValue] = useState("");
+  const [isClicked, setIsClicked] = useState(false);
+  const [isAvatarClicked, setIsAvatarClicked] = useState(false);
   return (
-    <div>
-      <input
-        type="text"
-        value={value}
-        onChange={(e) => {
-          setValue(e.target.value);
-        }}
-      />
-    </div>
+    <>
+      <div className="admin">
+        <div className={`admin-scroll ${isClicked ? "active" : ""}`}>
+          <h2>Lorem</h2>
+          <h2>Lorem</h2>
+          <h2>Lorem</h2>
+          <h2>Lorem</h2>
+          <h2>Lorem</h2>
+          <h2>Lorem</h2>
+        </div>
+        <div className="admin-nav">
+          <div className="admin-nav-area">
+            <MenuFoldOutlined
+              onClick={() => {
+                setIsClicked(!isClicked);
+              }}
+            />
+            <Avatar
+              shape="square"
+              size={30}
+              icon={<UserOutlined />}
+              onClick={() => setIsAvatarClicked(!isAvatarClicked)}
+            />
+          </div>
+          <div className="Carts">
+            <div className="card">
+              <div className="card-title">Lorem, ipsum.</div>
+              <strong>50%</strong>
+            </div>
+            <div className="card">
+              <div className="card-title">Lorem, ipsum.</div>
+              <strong>50%</strong>
+            </div>
+            <div className="card">
+              <div className="card-title">Lorem, ipsum.</div>
+              <strong>50%</strong>
+            </div>
+          </div>
+        </div>
+        <div className={`avatar-nav ${isAvatarClicked ? "active" : ""}`}>
+          <h2>Lorem</h2>
+          <h2>Lorem</h2>
+          <h2>Lorem</h2>
+          <h2>Lorem</h2>
+          <h2>Lorem</h2>
+          <h2>Lorem</h2>
+        </div>
+      </div>
+    </>
   );
 }
 
