0796c225761f63ce923fcc6359b14c5f4e8f17da
Bugra <seyitbugraerden@gmail.com>
2024-02-21 19:43:39 +0300

    Values Entering

--- src/Components/layouts/AdminMain.jsx
+++ src/Components/layouts/AdminMain.jsx
@@ -3,62 +3,62 @@ import React from "react";
 function AdminMain() {
   return (
     <div id="main-content">
-      <div class="container-fluid">
-        <div class="row clearfix row-deck mt-5">
-          <div class="col-lg-3 col-md-6 col-sm-6">
-            <div class="card bg-info">
-              <div class="body">
-                <div class="p-15 text-light">
+      <div className="container-fluid">
+        <div className="row clearfix row-deck mt-5">
+          <div className="col-lg-3 col-md-6 col-sm-6">
+            <div className="card bg-info">
+              <div className="body">
+                <div className="p-15 text-light">
                   <h3>2,120</h3>
                   <span>
                     Page Views{" "}
-                    <span class="font-12">
-                      <i class="fa fa-level-down"></i> 7%
+                    <span className="font-12">
+                      <i className="fa fa-level-down"></i> 7%
                     </span>
                   </span>
                 </div>
               </div>
             </div>
           </div>
-          <div class="col-lg-3 col-md-6 col-sm-6">
-            <div class="card bg-dark">
-              <div class="body">
-                <div class="p-15 text-light">
+          <div className="col-lg-3 col-md-6 col-sm-6">
+            <div className="card bg-dark">
+              <div className="body">
+                <div className="p-15 text-light">
                   <h3>2,120</h3>
                   <span>
                     Bounce rate{" "}
-                    <span class="font-12">
-                      <i class="fa fa-level-up"></i> 11%
+                    <span className="font-12">
+                      <i className="fa fa-level-up"></i> 11%
                     </span>
                   </span>
                 </div>
               </div>
             </div>
           </div>
-          <div class="col-lg-3 col-md-6 col-sm-6">
-            <div class="card bg-info">
-              <div class="body">
-                <div class="p-15 text-light">
+          <div className="col-lg-3 col-md-6 col-sm-6">
+            <div className="card bg-info">
+              <div className="body">
+                <div className="p-15 text-light">
                   <h3>2,120</h3>
                   <span>
                     Page Views{" "}
-                    <span class="font-12">
-                      <i class="fa fa-level-down"></i> 7%
+                    <span className="font-12">
+                      <i className="fa fa-level-down"></i> 7%
                     </span>
                   </span>
                 </div>
               </div>
             </div>
           </div>
-          <div class="col-lg-3 col-md-6 col-sm-6">
-            <div class="card bg-dark">
-              <div class="body">
-                <div class="p-15 text-light">
+          <div className="col-lg-3 col-md-6 col-sm-6">
+            <div className="card bg-dark">
+              <div className="body">
+                <div className="p-15 text-light">
                   <h3>2,120</h3>
                   <span>
                     Bounce rate{" "}
-                    <span class="font-12">
-                      <i class="fa fa-level-up"></i> 11%
+                    <span className="font-12">
+                      <i className="fa fa-level-up"></i> 11%
                     </span>
                   </span>
                 </div>
--- src/Components/pages/AdminHome.jsx
+++ src/Components/pages/AdminHome.jsx
@@ -1,70 +1,184 @@
-import React from "react";
+import React, { useState } from "react";
 import AdminNavbar from "../layouts/AdminNavbar";
 import AdminSidebar from "../layouts/AdminSidebar";
 import AdminRightBar from "../layouts/AdminRightBar";
 import { Input } from "antd";
+import { useSelector } from "react-redux";
+import { Button, Modal } from "antd";
 
 function AdminHome() {
+  const data = useSelector((state) => state.admin);
+  const [valSliderTitle, setValSliderTitle] = useState(
+    data.slider.title.content
+  );
+  const [valSliderDesc, setvalSliderDesc] = useState(data.slider.desc.content);
+  const [valMainMutedTitle, setvalMainMutedTitle] = useState(
+    data.main.mutedTitle.content
+  );
+  const [valMainTitle, setvalMainTitle] = useState(data.main.title.content);
+  const [valNewsMutedtitle, setValNewsMutedtitle] = useState(
+    data.news.mutedTitle.content
+  );
+  const [valNewsTitle, setValNewsTitle] = useState(data.news.title.content);
+  const [valDownTitle, setValDownTitle] = useState(data.subs.title.content);
+  const [valDownDesc, setValDownDesc] = useState(data.subs.desc.content);
+  //Modal Area
+  const [open, setOpen] = useState(false);
+  const modalText = "Your changes will be saved";
+
   return (
     <>
       <AdminNavbar />
       <AdminSidebar />
       <AdminRightBar />
       <div id="main-content">
-        <div class="container-fluid mt-5">
+        <div className="container-fluid mt-5">
           <h5 className="mb-5">Home Page</h5>
           <strong className="mt-5">Slider Area</strong>
-          <div class="row clearfix row-deck mt-3">
-            <div class="col-sm-2">Slider Title</div>
-            <div class="col-sm-3">
-              <Input placeholder="Enter New Value" />
+          <div className="row clearfix row-deck mt-3">
+            <div className="col-sm-2">Slider Title</div>
+            <div className="col-sm-3">
+              <Input
+                placeholder="Enter New Value"
+                value={valSliderTitle}
+                onChange={(e) => {
+                  setValSliderTitle(e.target.value);
+                }}
+              />
+            </div>
+            <div className="col-sm-4">
+              <i>{data.slider.title.content}</i>
             </div>
           </div>
-          <div class="row clearfix row-deck mt-3 mb-5">
-            <div class="col-sm-2">Slider Description</div>
-            <div class="col-sm-3">
-              <Input placeholder="Enter New Value" />
+          <div className="row clearfix row-deck mt-3 mb-5">
+            <div className="col-sm-2">Slider Description</div>
+            <div className="col-sm-3">
+              <Input
+                value={valSliderDesc}
+                placeholder="Enter New Value"
+                onChange={(e) => {
+                  setvalSliderDesc(e.target.value);
+                }}
+              />
+            </div>
+            <div className="col-sm-4">
+              <i>{data.slider.desc.content}</i>
             </div>
           </div>
           <strong>Main Area</strong>
-          <div class="row clearfix row-deck mt-3">
-            <div class="col-sm-2">Main Muted Title</div>
-            <div class="col-sm-3">
-              <Input placeholder="Enter New Value" />
+          <div className="row clearfix row-deck mt-3">
+            <div className="col-sm-2">Main Muted Title</div>
+            <div className="col-sm-3">
+              <Input
+                value={valMainMutedTitle}
+                onChange={(e) => {
+                  setvalMainMutedTitle(e.target.value);
+                }}
+                placeholder="Enter New Value"
+              />
+            </div>
+            <div className="col-sm-3">
+              <i>{data.main.mutedTitle.content}</i>
             </div>
           </div>
-          <div class="row clearfix row-deck mt-3 mb-5">
-            <div class="col-sm-2">Main Title</div>
-            <div class="col-sm-3">
-              <Input placeholder="Enter New Value" />
+          <div className="row clearfix row-deck mt-3 mb-5">
+            <div className="col-sm-2">Main Title</div>
+            <div className="col-sm-3">
+              <Input
+                value={valMainTitle}
+                onChange={(e) => {
+                  setvalMainTitle(e.target.value);
+                }}
+                placeholder="Enter New Value"
+              />
+            </div>
+            <div className="col-sm-3">
+              <i>{data.main.title.content}</i>
             </div>
           </div>
           <strong>News Area</strong>
-          <div class="row clearfix row-deck mt-3">
-            <div class="col-sm-2">News Muted Title</div>
-            <div class="col-sm-3">
-              <Input placeholder="Enter New Value" />
+          <div className="row clearfix row-deck mt-3">
+            <div className="col-sm-2">News Muted Title</div>
+            <div className="col-sm-3">
+              <Input
+                value={valNewsMutedtitle}
+                onChange={(e) => {
+                  setValNewsMutedtitle(e.target.value);
+                }}
+                placeholder="Enter New Value"
+              />
+            </div>
+            <div className="col-sm-3">
+              <i>{data.main.mutedTitle.content}</i>
             </div>
           </div>
-          <div class="row clearfix row-deck mt-3 mb-5">
-            <div class="col-sm-2">New Title</div>
-            <div class="col-sm-3">
-              <Input placeholder="Enter New Value" />
+          <div className="row clearfix row-deck mt-3 mb-5">
+            <div className="col-sm-2">New Title</div>
+            <div className="col-sm-3">
+              <Input
+                value={valNewsTitle}
+                onChange={(e) => {
+                  setValNewsTitle(e.target.value);
+                }}
+                placeholder="Enter New Value"
+              />
+            </div>
+            <div className="col-sm-3">
+              <i>{data.main.title.content}</i>
             </div>
           </div>
           <strong>Down Slide Area</strong>
-          <div class="row clearfix row-deck mt-3">
-            <div class="col-sm-2">Down Slide Title</div>
-            <div class="col-sm-3">
-              <Input placeholder="Enter New Value" />
+          <div className="row clearfix row-deck mt-3">
+            <div className="col-sm-2">Down Slide Title</div>
+            <div className="col-sm-3">
+              <Input
+                value={valDownTitle}
+                onChange={(e) => {
+                  setValDownTitle(e.target.value);
+                }}
+                placeholder="Enter New Value"
+              />
+            </div>
+            <div className="col-sm-3">
+              <i>{data.subs.title.content}</i>
             </div>
           </div>
-          <div class="row clearfix row-deck mt-3">
-            <div class="col-sm-2">Down Slide Description</div>
-            <div class="col-sm-3">
-              <Input placeholder="Enter New Value" />
+          <div className="row clearfix row-deck mt-3 mb-5">
+            <div className="col-sm-2">Down Slide Description</div>
+            <div className="col-sm-3">
+              <Input
+                value={valDownDesc}
+                onChange={(e) => {
+                  setValDownDesc(e.target.value);
+                }}
+                placeholder="Enter New Value"
+              />
+            </div>
+            <div className="col-sm-3">
+              <i>{data.subs.desc.content}</i>
             </div>
           </div>
+
+          <Button
+            type="primary"
+            onClick={() => {
+              setOpen(true);
+            }}
+          >
+            Save
+          </Button>
+          <Modal
+            title="Are You Sure ?"
+            open={open}
+            onOk={() => {
+              setOpen(false);
+            }}
+            onCancel={() => {
+              setOpen(false);
+            }}
+          >
+            <p>{modalText}</p>
+          </Modal>
         </div>
       </div>
     </>
