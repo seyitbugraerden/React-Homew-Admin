fd0838e2cbddbcf9489290958d8190534d5a1f02
Bugra <seyitbugraerden@gmail.com>
2024-02-25 18:35:27 +0300

    Admin Page Issues

--- src/Components/pages/AdminHome.jsx
+++ src/Components/pages/AdminHome.jsx
@@ -2,148 +2,16 @@ import React, { useRef, useState } from "react";
 import AdminNavbar from "../layouts/AdminNavbar";
 import AdminSidebar from "../layouts/AdminSidebar";
 import AdminRightBar from "../layouts/AdminRightBar";
-import { Input } from "antd";
-import { useSelector } from "react-redux";
-import { Button, Modal } from "antd";
-import axios from "axios";
+
+import AdminInputArea from "../layouts/AdminInputArea";
 
 function AdminHome() {
-  const data = useSelector((state) => state.admin);
-  //Modal Area
-  const [open, setOpen] = useState(false);
-  const modalText = "Your changes will be saved";
-  //UseRef Area
-  const valSliderTitle = useRef(null);
-  const valSliderDesc = useRef(null);
-  const valMainMutedTitle = useRef(null);
-  const valMainTitle = useRef(null);
-  const valNewsMutedtitle = useRef(null);
-  const valNewsTitle = useRef(null);
-  const valDownTitle = useRef(null);
-  const valDownDesc = useRef(null);
-  const onOk = () => {
-    setOpen(false);
-    axios.put("http://localhost:3000/navbarItems/Header", {
-      title: "New",
-    });
-  };
-
   return (
     <>
       <AdminNavbar />
       <AdminSidebar />
       <AdminRightBar />
-      <div id="main-content">
-        <div className="container-fluid mt-5">
-          <h5 className="mb-5">Home Page</h5>
-          <strong className="mt-5">Slider Area</strong>
-          <div className="row clearfix row-deck mt-3">
-            <div className="col-sm-2">Slider Title</div>
-            <div className="col-sm-3">
-              <Input ref={valSliderTitle} placeholder="Enter New Value" />
-            </div>
-            <div className="col-sm-4">
-              <i>{data.slider.title.content}</i>
-            </div>
-          </div>
-          <div className="row clearfix row-deck mt-3 mb-5">
-            <div className="col-sm-2">Slider Description</div>
-            <div className="col-sm-3">
-              <Input ref={valSliderDesc} placeholder="Enter New Value" />
-            </div>
-            <div className="col-sm-4">
-              <i>{data.slider.desc.content}</i>
-            </div>
-          </div>
-          <strong>Main Area</strong>
-          <div className="row clearfix row-deck mt-3">
-            <div className="col-sm-2">Main Muted Title</div>
-            <div className="col-sm-3">
-              <Input ref={valMainMutedTitle} placeholder="Enter New Value" />
-            </div>
-            <div className="col-sm-3">
-              <i>{data.main.mutedTitle.content}</i>
-            </div>
-          </div>
-          <div className="row clearfix row-deck mt-3 mb-5">
-            <div className="col-sm-2">Main Title</div>
-            <div className="col-sm-3">
-              <Input ref={valMainTitle} placeholder="Enter New Value" />
-            </div>
-            <div className="col-sm-3">
-              <i>{data.main.title.content}</i>
-            </div>
-          </div>
-          <strong>News Area</strong>
-          <div className="row clearfix row-deck mt-3">
-            <div className="col-sm-2">News Muted Title</div>
-            <div className="col-sm-3">
-              <Input ref={valNewsMutedtitle} placeholder="Enter New Value" />
-            </div>
-            <div className="col-sm-3">
-              <i>{data.main.mutedTitle.content}</i>
-            </div>
-          </div>
-          <div className="row clearfix row-deck mt-3 mb-5">
-            <div className="col-sm-2">New Title</div>
-            <div className="col-sm-3">
-              <Input ref={valNewsTitle} placeholder="Enter New Value" />
-            </div>
-            <div className="col-sm-3">
-              <i>{data.main.title.content}</i>
-            </div>
-          </div>
-          <strong>Down Slide Area</strong>
-          <div className="row clearfix row-deck mt-3">
-            <div className="col-sm-2">Down Slide Title</div>
-            <div className="col-sm-3">
-              <Input ref={valDownTitle} placeholder="Enter New Value" />
-            </div>
-            <div className="col-sm-3">
-              <i>{data.subs.title.content}</i>
-            </div>
-          </div>
-          <div className="row clearfix row-deck mt-3 mb-5">
-            <div className="col-sm-2">Down Slide Description</div>
-            <div className="col-sm-3">
-              <Input ref={valDownDesc} placeholder="Enter New Value" />
-            </div>
-            <div className="col-sm-3">
-              <i>{data.subs.desc.content}</i>
-            </div>
-          </div>
-
-          <Button
-            className="my-5"
-            type="primary"
-            onClick={() => {
-              setOpen(true);
-              console.log(valSliderTitle.current?.input.value);
-              console.log(valSliderDesc.current?.input.value);
-              console.log(valMainMutedTitle.current?.input.value);
-              console.log(valMainTitle.current?.input.value);
-              console.log(valNewsMutedtitle.current?.input.value);
-              console.log(valNewsTitle.current?.input.value);
-              console.log(valDownTitle.current?.input.value);
-              console.log(valDownDesc.current?.input.value);
-            }}
-          >
-            Save
-          </Button>
-          <Modal
-            title="Are You Sure ?"
-            open={open}
-            onOk={() => {
-              onOk();
-            }}
-            onCancel={() => {
-              setOpen(false);
-            }}
-          >
-            <p>{modalText}</p>
-          </Modal>
-        </div>
-      </div>
+      <AdminInputArea />
     </>
   );
 }
