92a63443392a71c55183fb69faa66c4c25054971
Bugra <seyitbugraerden@gmail.com>
2024-02-25 19:47:06 +0300

    Params Delete

--- src/Components/layouts/Admin/AdminInputArea.jsx
+++ src/Components/layouts/Admin/AdminInputArea.jsx
@@ -1,7 +1,6 @@
 import { Input } from "antd";
 import React from "react";
 import { Button, Modal } from "antd";
-import { useParams } from "react-router-dom";
 
 function AdminInputArea() {
   return (
